969f9a6c5d48ab108b8a8583cec11959ac32563c
Nisha Tanwar <103480687+NishaTanwar20@users.noreply.github.com>
2022-12-27 21:33:37 +0530

    Update counterContainer.js

--- src/components/counterContainer.js
+++ src/components/counterContainer.js
@@ -15,4 +15,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(counter)
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
